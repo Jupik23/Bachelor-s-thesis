50602992870ee9c4252af56f0809988155de9ec6
Jupik23 <jupekkekw@gmail.com>
2025-11-13 17:46:41 +0100

    generating and displaying dependent plan

--- back/app/api/routes/dependant.py
+++ back/app/api/routes/dependant.py
@@ -1,4 +1,3 @@
-# W pliku: app/routes/dependent_routes.py
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
@@ -7,7 +6,8 @@ from app.database.database import get_database
 from app.utils.jwt import get_current_user
 from app.services.dependant import DependentService
 from app.schemas.dependent import DependentCreateRequest
-from app.schemas.user import User as UserSchema # Używamy jako model odpowiedzi
+from app.schemas.user import User as UserSchema
+from app.schemas.plan import PlanResponse
 
 router = APIRouter(
     prefix="/api/v1/dependents",
@@ -47,3 +47,28 @@ async def get_my_dependents(
     service = DependentService(db)
     dependents = service.get_my_dependents(carer_id=current_user.id)
     return dependents
+
+@router.get("/{dependent_id}/plan/today", response_model=PlanResponse)
+async def get_dependent_plan_today(
+    dependent_id: int,
+    db: Session = Depends(get_database),
+    current_user: dict = Depends(get_current_user)
+):
+    service = DependentService(db)
+    
+    return await service.get_dependent_plan(
+        carer_id=current_user.id,
+        dependent_id=dependent_id
+    )
+
+@router.post("/{dependent_id}/plan/generate", response_model=PlanResponse)
+async def generate_dependent_plan(
+    dependent_id: int,
+    db: Session = Depends(get_database),
+    current_user: dict = Depends(get_current_user)
+):
+    service = DependentService(db)
+    return await service.generate_dependent_plan(
+        carer_id=current_user.id,
+        dependent_id=dependent_id
+    )
--- back/app/services/dependant.py
+++ back/app/services/dependant.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
 from typing import List
+from fastapi import status, HTTPException
 from app.schemas.dependent import DependentCreateRequest
 from app.schemas.user_auth import UserAuthCreate
+from app.schemas.plan import PlanResponse
 from app.models.user import User
 from app.crud import user as crud_user
 from app.crud import user_auth as crud_user_auth
 from app.crud import care_relation as crud_care_relation
 from app.services.user import UserService
+from app.services.plan import PlanCreationService
 
 class DependentService:
     def __init__(self, db: Session):
@@ -50,3 +53,56 @@ class DependentService:
     def get_my_dependents(self, carer_id: int) -> List[User]:
         dependents = crud_care_relation.get_dependents_by_carer_id(self.db, carer_id=carer_id)
         return dependents
+    
+    async def get_dependent_plan(self, carer_id: int, dependent_id: int) -> PlanResponse:
+        has_relation = crud_care_relation.check_relation(
+            db=self.db,
+            carer_id=carer_id,
+            patient_id=dependent_id
+        )
+        if not has_relation:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to view this dependent's plan."
+            )
+        try:
+            plan_service = PlanCreationService(self.db)
+            plan_data = await plan_service.get_todays_plan_for_user(user_id=dependent_id)
+            return plan_data
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Error while getting dependent's plan: {e}"
+            )
+        
+    async def generate_dependent_plan(self, carer_id: int, dependent_id: int) -> PlanResponse:
+        has_relation = crud_care_relation.check_relation(
+            db=self.db,
+            carer_id=carer_id,
+            patient_id=dependent_id
+        )
+        if not has_relation:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to generate this dependent's plan."
+            )
+        
+        try:
+            plan_service = PlanCreationService(self.db)
+
+            plan_data = await plan_service.generate_and_save_plan(
+                created_by_id=carer_id,
+                user_id=dependent_id,
+                time_frame="day"
+            )
+            return plan_data
+        except ValueError as ve:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(ve) 
+            )
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Error while generating dependent's plan: {e}"
+            )
